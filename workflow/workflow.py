49a00204bd8dc7e491e29ec12a33e653f3a70a61
Sileadim <christopher@omnius.com>
2020-08-18 16:23:18 +0200

    started workflow

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -1,4 +1,3 @@
-
 # Worflow does the following:
 # Read collection dir and csv
 # Read split files
@@ -9,7 +8,7 @@
 # Convert test set to jsons
 # Run mleval on predicted jsons
 
-
+import math
 import logging
 import glob
 from convert import check_gt_and_generate_copynet_file, STATUS
@@ -18,7 +17,7 @@ from os.path import join
 import pandas as pd
 import sys
 from joblib import Parallel, delayed
-from jsonargparse import ArgumentParser
+from jsonargparse import ArgumentParser, ActionPath, Path
 import json
 import subprocess
 from allennlp.training.metrics.information_extraction import parse_json
@@ -41,7 +40,7 @@ OCR_NAME = "ocr-page.xml"
 DEFAULT_CONFIG = "default_copynet.json"
 MODEL = "model"
 
-DEFAULT_FIELDS = {
+ALL_FIELDS = [
     "ClaimNumber",
     "LossDate",
     "LossStateCd",
@@ -56,14 +55,25 @@ DEFAULT_FIELDS = {
     "DOB",
     "SSN",
     "Address1",
-}
+]
 
 
-def get_config_parser():
-
-    parser = ArgumentParser()
-    # TODO
-    return parser
+DEFAULT_FIELDS = [
+    "ClaimNumber",
+    "LossDate",
+    "LossStateCd",
+    "Policy",
+    "RegulatoryClaimNumber",
+    "ConvertedClaimNumber",
+    "VIN",
+    "InsuredName",
+    "CompanyName",
+    "FirstName",
+    "LastName",
+    "DOB",
+    "SSN",
+    "Address1",
+]
 
 
 def update_default_config(default_path, train_path, val_path, max_target_length, intermediate_path):
@@ -71,7 +81,8 @@ def update_default_config(default_path, train_path, val_path, max_target_length,
     d = json.load(open(default_path))
     d["train_data_path"] = train_path
     d["validation_data_path"] = val_path
-    d["model"]["max_decoding_steps"] = max_target_length + 1
+    # give 10 percent slack, so that if we copy more for one field the decoding does not stop
+    d["model"]["max_decoding_steps"] = math.floor(max_target_length * 1.1)
     json.dump(d, open(intermediate_path, "w"), ensure_ascii=False, indent=4)
     logger.info(f"dumped copynet config to {intermediate_path}")
 
@@ -170,15 +181,18 @@ def convert_prediction_to_jsons(prediction_path, intermediate_path, uuids):
     logger.info("Converted to proper jsons.")
     return predicted_json_paths
 
+
 def run_evaluation(predicted_json_paths, report_dir):
 
     parser = InformationExtractionEvaluator.get_config_parser()
     cfg = parser.parse_args(["--doc_id", "parent-dir"])
-    evaluator = InformationExtractionEvaluator(cfg=cfg) 
-    gt_json_paths = [ pred.replace("pred","gt") for pred in predicted_json_paths  ]
+    evaluator = InformationExtractionEvaluator(cfg=cfg)
+    gt_json_paths = [pred.replace("pred", "gt") for pred in predicted_json_paths]
     evaluator.generate_report(outdir=report_dir, gt=gt_json_paths, pr=predicted_json_paths)
     logger.info("Evaluation done")
-if __name__ == "__main__":
+
+
+def run():
 
     splits, max_source_length, max_target_length = generate_copynet_files()
     print(splits)
@@ -195,11 +209,127 @@ if __name__ == "__main__":
     # if not success:
     #    exit()
     prediction_path = join(OUTPUT_DIR, INTERMEDIATE, "test_predictions.txt")
-    #success = predict(model_dir, splits["test"]["copynet"], prediction_path)
-    #if not success:
+    # success = predict(model_dir, splits["test"]["copynet"], prediction_path)
+    # if not success:
     #    exit()
     print(prediction_path)
     predicted_json_paths = convert_prediction_to_jsons(
         prediction_path, join(OUTPUT_DIR, INTERMEDIATE, COLLECTION_ID), splits["test"]["paths"]
     )
     run_evaluation(predicted_json_paths, join(OUTPUT_DIR, "report"))
+
+
+class Workflow:
+    @staticmethod
+    def get_config_parser():
+
+        parser = ArgumentParser()
+        parser.add_argument("--output_dir", action=ActionPath(mode="dw"))
+        parser.add_argument("--collection", action=ActionPath(mode="dr"))
+        parser.add_argument("--splits.train", action=ActionPath(mode="fr"))
+        parser.add_argument("--splits.val", action=ActionPath(mode="fr"))
+        parser.add_argument("--splits.test", action=ActionPath(mode="fr"))
+        parser.add_argument("--csv", action=ActionPath(mode="fr"))
+        parser.add_argument(
+            "--default_copynet_config",
+            action=ActionPath(mode="fr"),
+            default=Path("default_copynet.json"),
+        )
+        parser.add_argument("--ocr_name", type=str, default="ocr-page.xml")
+        parser.add_argument("--fields", nargs="+", choices=ALL_FIELDS, default=DEFAULT_FIELDS)
+        parser.add_argument("--recover", action="store_true")
+        # TODO
+        return parser
+
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.intermediate = "intermediate"
+
+    def run(self):
+
+        self.generate_copynet_files()
+        self.update_default_config()
+        # model_dir = join(OUTPUT_DIR, MODEL)
+        # success = train_model(cfg_path, model_dir)
+        # if not success:
+        #    exit()
+        # prediction_path = join(OUTPUT_DIR, INTERMEDIATE, "test_predictions.txt")
+        # success = predict(model_dir, splits["test"]["copynet"], prediction_path)
+        # if not success:
+        #    exit()
+        # print(prediction_path)
+        # predicted_json_paths = convert_prediction_to_jsons(
+        #    prediction_path, join(OUTPUT_DIR, INTERMEDIATE, COLLECTION_ID), splits["test"]["paths"]
+        # )
+        # run_evaluation(predicted_json_paths, join(OUTPUT_DIR, "report"))
+
+    def generate_copynet_files(self):
+        max_source_length, max_target_length = 0, 0
+        splits = {}
+        df = pd.read_csv(self.cfg.csv.path, dtype=str)
+        SPLITS = {
+            "train": self.cfg.splits.train.path,
+            "val": self.cfg.splits.val.path,
+            "test": self.cfg.splits.test.path,
+        }
+        for split, file in SPLITS.items():
+            with open(join(INPUT_DIR, file)) as f:
+                uuids = f.read().splitlines()
+                results = Parallel(n_jobs=1)(
+                    delayed(check_gt_and_generate_copynet_file)(
+                        join(self.cfg.output_dir.path, self.intermediate, "data", uuid),
+                        join(self.cfg.collection.path, uuid, self.cfg.ocr_name),
+                        df,
+                        self.cfg.fields,
+                    )
+                    for uuid in uuids
+                )
+                ok_count = 0
+                split_file = join(self.cfg.output_dir.path, self.intermediate, split + ".txt")
+                splits[split] = {}
+                splits[split]["copynet"] = split_file
+                splits[split]["paths"] = []
+                with open(split_file, "w") as f:
+                    for res in results:
+                        if res[1] == STATUS.OK:
+                            ok_count += 1
+                            max_source_length = max(max_source_length, res[0][1])
+                            max_target_length = max(max_target_length, res[0][2])
+                            splits[split]["paths"].append(
+                                os.path.basename(os.path.dirname(res[0][0]))
+                            )
+                            with open(res[0][0]) as g:
+                                line = g.read()
+                                f.write(line)
+                logger.info(f"{ok_count} ok of {len(uuids)} in {split} ")
+                if ok_count == 0:
+                    raise ValueError(f"Empty split {split}")
+        logger.info(
+            f"max_source_length: {max_source_length}, max_target_length {max_target_length}"
+        )
+
+        self.splits, self.max_source_length, self.max_target_length = (
+            splits,
+            max_source_length,
+            max_target_length,
+        )
+
+    def update_default_config(self):
+
+        d = json.load(open(self.cfg.default_copynet_config.path))
+        d["train_data_path"] = self.splits["train"]["copynet"]
+        d["validation_data_path"] = self.splits["val"]["copynet"]
+        # give 10 percent slack, so that if we copy more for one field the decoding does not stop
+        d["model"]["max_decoding_steps"] = math.floor(self.max_target_length * 1.1)
+        out_cfg_path = join(self.cfg.output_dir.path, self.intermediate, "config.json")
+        json.dump(d, open(out_cfg_path, "w"), ensure_ascii=False, indent=4)
+        logger.info(f"dumped copynet config to {out_cfg_path}")
+
+
+if __name__ == "__main__":
+
+    parser = Workflow.get_config_parser()
+    cfg = parser.parse_args()
+    print(cfg)
+    workflow = Workflow(cfg=cfg)
+    workflow.run()
